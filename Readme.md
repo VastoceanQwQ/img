3cbd939617dd0e0c499ed6ec8cacdbb8dbdd76ca
haohanhaiyang <1611189544@qq.com>
2022-05-31 14:10:07 +0800

    20220531_1

--- Readme.md
+++ Readme.md
@@ -3,7 +3,6 @@
 调用地址: https://img.hhhy.link/random.php
 
 图片托管仓库: https://github.com/haohanhaiyang/img
-
 国内镜像仓库:  https://gitee.com/haohanhaiyang/img
 
-现收录161张壁纸|更新日期: 2022.05.30
+现收录166张壁纸|更新日期: 2022.05.30
--- random.php
+++ random.php
@@ -1,10 +1,12 @@
 <?php
+header('Access-Control-Allow-Origin:*');
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
 $num = mt_rand(1,166);
 //拼接图片地址
+//$picpath = "https://fastly.jsdelivr.net/gh/haohanhaiyang/img@main/nature/".$num.".jpg";
 $picpath = "https://haohanhaiyang.gitee.io/img/nature/".$num.".jpg";
 //重定位到图片
-die(header("Location: $picpath"));
+die(header("Location: $picpath"))
 ?>
